e197bfadec56c81b06f7f8196d4e64ce3497791e
Pradip <pradip4799@gmail.com>
2023-07-25 09:37:09 +0530

    Largest of three number

---
+++ LargestNumber.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class LargestNumber {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Enter the first number: ");
+        double num1 = input.nextDouble();
+        
+        System.out.print("Enter the second number: ");
+        double num2 = input.nextDouble();
+        
+        System.out.print("Enter the third number: ");
+        double num3 = input.nextDouble();
+        
+        double largest = num1;
+        
+        if (num2 > largest) {
+            largest = num2;
+        }
+        
+        if (num3 > largest) {
+            largest = num3;
+        }
+        
+        System.out.println("The largest number is: " + largest);
+    }
+}
